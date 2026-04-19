7cbf25ccf4715ba6be55d4834c24993a075dfeb2
hw5u <jeekwol.dev@gmail.com>
2026-04-19 19:42:23 -0400

    brang back sound

--- src/mapManager.lua
+++ src/mapManager.lua
@@ -26,9 +26,9 @@ function mapManager:update()
 				self.current = i
 				self.background = love.graphics.newImage(maps[mapManager.current].background)
 
-				--self.music:stop()
+				self.music:stop()
 				self.music = love.audio.newSource(maps[mapManager.current].music, "stream")
-				--self.music:play()
+				self.music:play()
 			end
 			break
 		end
